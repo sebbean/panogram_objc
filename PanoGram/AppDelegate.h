1796bb7defa6d9b851f5ec2911a3438e7ad53083
Sebastian Bean <sebastian@ginlanemedia.com>
2014-11-03 19:20:39 -0500

    Initial Commit

---
+++ PanoGram/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  PanoGram
+//
+//  Created by Sebastian Bean on 11/3/14.
+//  Copyright (c) 2014 Gin Lane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ PanoGram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PanoGram
+//
+//  Created by Sebastian Bean on 11/3/14.
+//  Copyright (c) 2014 Gin Lane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
